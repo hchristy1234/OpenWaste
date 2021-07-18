0fdc4806ef238643ca207765bbf665c0e3892f14
hchristy1234 <hchristy1234@gmail.com>
2021-07-17 17:32:49 -0700

    add javascript to add-home function

--- public/script.js
+++ public/script.js
@@ -1,11 +1,24 @@
 const submit = $('.submit')
 const container = $('.searchContainer')
 
+const submithome = $('.submithome')
+const homecontainer = $('.homeAddContainer')
+
 submit.on("click", findHome)
 
 function findHome() {
     container.html("")
     container.append(`
         <p class="text-white text-center mt-4">No Homes Found</p>
+    `)
+}
+
+
+submithome.on("click", addhome)
+
+function addhome() {
+    homecontainer.html("")
+    homecontainer.append(`
+        <p class="text-white text-center text-large mt-4">Thank you for adding your home and helping the environment!</p>
     `)
 }
